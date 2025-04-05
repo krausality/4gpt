5b56ac9e53c3a188c2e3fec6dc8beeaa72080041
Krausality <martinkrausemedia@gmail.com>
2025-04-05 22:43:07 +0200

    some changes

--- setup.py
+++ setup.py
@@ -6,16 +6,18 @@ setup(
     packages=find_packages(),
     include_package_data=True,  # Ensure package data (like config.json) is included
     package_data={
-        'twogpt': ['config.json'],  # Specify that config.json is included in the twogpt package
+        'forgpt': ['config.json'],  # Specify that config.json is included in the twogpt package
     },
-    install_requires=[],  # Add any dependencies here
+    install_requires=[
+        'dir-tree @ git+https://github.com/krausality/dir_tree.git',
+    ],
     entry_points={
         'console_scripts': [
-            '4gpt=twogpt.core:main',  # Adds `4gpt` command to the CLI
+            '4gpt=forgpt.core:main',  # Adds `4gpt` command to the CLI
         ],
     },
     author='krausality',
-    author_email='krausality42@gmail.com',
+    author_email='github@krausality.com',
     description='A CLI tool for generating an `allfiles.txt` report of a directory, with inclusion and exclusion rules managed through a `.gptignore` file and a centralized config.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
